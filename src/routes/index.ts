92578ad4e16b4277ad345214ee32eda035099227
zapodask <joaopedroemerim@gmail.com>
2023-10-21 19:53:57 -0300

    feat: docker

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,74 +32,74 @@ routes.get("/users", async (req, res) => {
 })
 
 routes.post("/musics/add", async (req, res) => {
-  callController(res, async () => MusicRepository.addMusics(req.body))
+callController(res, async () => MusicRepository.addMusics(req.body))
+})
 
-  routes.get("/users/playlists", async (req, res) => {
-    callController(res, new UsersController(req, res).getUserPlaylists)
-  })
+routes.get("/users/playlists", async (req, res) => {
+  callController(res, new UsersController(req, res).getUserPlaylists)
+})
 
-  routes.post("/users/create", async (req, res) => {
-    callController(res, new UsersController(req, res).createUser)
-  })
+routes.post("/users/create", async (req, res) => {
+  callController(res, new UsersController(req, res).createUser)
+})
 
-  routes.post("/users/login", async (req, res) => {
-    callController(res, new UsersController(req, res).loginUser)
-  })
+routes.post("/users/login", async (req, res) => {
+  callController(res, new UsersController(req, res).loginUser)
+})
 
-  routes.get("/users/favorite-musics", async (req, res) => {
-    callController(res, new UsersController(req, res).getFavoriteMusics)
-  })
+routes.get("/users/favorite-musics", async (req, res) => {
+  callController(res, new UsersController(req, res).getFavoriteMusics)
+})
 
-  routes.get("/user/profile", middleware, async (req, res) => {
-    callController(res, new UsersController(req, res).getUserProfile)
-  })
+routes.get("/user/profile", middleware, async (req, res) => {
+  callController(res, new UsersController(req, res).getUserProfile)
+})
 
-  routes.get("/musics", async (req, res) => {
-    callController(res, new MusicsController(req, res).getMusics)
-  })
+routes.get("/musics", async (req, res) => {
+  callController(res, new MusicsController(req, res).getMusics)
+})
 
-  routes.patch("/musics/like-or-desliked", async (req, res) => {
-    callController(res, new MusicsController(req, res).likeOrDeslike)
-  })
+routes.patch("/musics/like-or-desliked", async (req, res) => {
+  callController(res, new MusicsController(req, res).likeOrDeslike)
+})
 
-  routes.get("/musics/liked", async (req, res) => {
-    callController(res, new MusicsController(req, res).getLikedMusics)
-  })
+routes.get("/musics/liked", async (req, res) => {
+  callController(res, new MusicsController(req, res).getLikedMusics)
+})
 
-  routes.get("/getMusicToId/:id", async (req, res) => {
-    callController(res, new MusicsController(req, res).getMusicsToId)
-  })
+routes.get("/getMusicToId/:id", async (req, res) => {
+  callController(res, new MusicsController(req, res).getMusicsToId)
+})
 
-  routes.post("/musics/addmusictoplaylist", async (req, res) => {
-    callController(res, new MusicsController(req, res).addMusicsToPlaylist)
-  })
+routes.post("/musics/addmusictoplaylist", async (req, res) => {
+  callController(res, new MusicsController(req, res).addMusicsToPlaylist)
+})
 
-  routes.get("/playlists", async (req, res) => {
-    callController(res, new PlaylistsController(req, res).getPlaylists)
-  })
+routes.get("/playlists", async (req, res) => {
+  callController(res, new PlaylistsController(req, res).getPlaylists)
+})
 
-  routes.get("/playlist/:id", async (req, res) => {
-    callController(res, new PlaylistsController(req, res).getPlaylists)
-  })
+routes.get("/playlist/:id", async (req, res) => {
+  callController(res, new PlaylistsController(req, res).getPlaylists)
+})
 
-  routes.post("/playlists/create", middleware, async (req, res) => {
-    callController(res, new PlaylistsController(req, res).createPlaylists)
-  })
+routes.post("/playlists/create", middleware, async (req, res) => {
+  callController(res, new PlaylistsController(req, res).createPlaylists)
+})
 
-  routes.patch("/playlists/updated", async (req, res) => {
-    callController(res, new PlaylistsController(req, res).uptadePlaylistStatus)
-  })
+routes.patch("/playlists/updated", async (req, res) => {
+  callController(res, new PlaylistsController(req, res).uptadePlaylistStatus)
+})
 
-  routes.delete("/playlists/delete/:playlist_id", async (req, res) => {
-    callController(res, new PlaylistsController(req, res).deletePlaylist)
-  })
+routes.delete("/playlists/delete/:playlist_id", async (req, res) => {
+  callController(res, new PlaylistsController(req, res).deletePlaylist)
+})
 
-  routes.delete("/playlists/delete-music", async (req, res) => {
-    callController(res, new PlaylistsController(req, res).deleteMusicToPlaylist)
-  })
+routes.delete("/playlists/delete-music", async (req, res) => {
+  callController(res, new PlaylistsController(req, res).deleteMusicToPlaylist)
+})
 
-  routes.get("/playlists/musics/:playlist_id", async (req, res) => {
-    callController(res, new PlaylistsController(req, res).getPlaylistsMusics)
-  })
+routes.get("/playlists/musics/:playlist_id", async (req, res) => {
+  callController(res, new PlaylistsController(req, res).getPlaylistsMusics)
 })
 export default routes
